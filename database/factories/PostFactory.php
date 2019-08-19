74a76b2ce6d7a170e4aa7b35b7b1b1614a74b68f
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-19 07:06:08 -0700

    Updated post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,7 +5,7 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Post::class, function (Faker $faker) {
     return [
         'user_id' => function () {
             return factory(App\User::class)->create()->id;
